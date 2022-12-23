c085c6128e3b0eac1c735cb86c0bc475d2b2445f
BayuShaburRasyidin <shaburbayu@gmail.com>
2022-12-23 22:48:46 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+     
+      <Skills />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Skills.jsx
@@ -0,0 +1,193 @@
+import React from 'react';
+
+
+import foto from '../assets/vidio.png';
+
+const Skills = () => {
+  return (
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full pt-20'>
+          <div class="w-20">
+              
+              
+          </div>
+          
+     </div>
+
+     <div class="w-20 ml-[85px] pt-10">
+     <h4 class="font-bold bg-red-700 px-2 py-1 rounded-full"> populer</h4>
+     </div>
+     
+     <div class="container mx-auto text-center px-6 sm:flex sm:gap-6 mt-10 ">
+      <div class="rounded-md shadow-lg overflow-hidden mb-10 bg-white ">
+       <div><img src={foto} alt='vidio' class="w-full"></img></div>
+        
+        <div class="px-6 py-2">
+          <div class="font-bold text-xl mb-2 text-slate-700 ">
+            title
+          </div>
+            <p class="text-sm text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, perferendis?
+            </p>
+          
+
+        </div>
+      </div>
+      <div class="rounded-md shadow-lg overflow-hidden mb-10 ">
+      <div><img src={foto} alt='vidio' class="w-full"></img></div>
+        <div class="px-6 py-2">
+          <div class="font-bold text-xl mb-2 text-slate-700 ">
+            title
+          </div>
+            <p class="text-sm text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, perferendis?
+            </p>
+          
+
+        </div>
+      </div>
+      <div class="rounded-md shadow-lg overflow-hidden mb-10">
+      <div><img src={foto} alt='vidio' class="w-full"></img></div>
+        <div class="px-6 py-2">
+          <div class="font-bold text-xl mb-2 text-slate-700 ">
+            title
+          </div>
+            <p class="text-sm text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, perferendis?
+            </p>
+          
+
+        </div>
+      </div>
+      <div class="rounded-md shadow-lg overflow-hidden mb-10">
+      <div><img src={foto} alt='vidio' class="w-full"></img></div>
+        <div class="px-6 py-2">
+          <div class="font-bold text-xl mb-2 text-slate-700 ">
+            title
+          </div>
+            <p class="text-sm text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, perferendis?
+            </p>
+          
+
+        </div>
+      </div>
+     </div>
+
+     <div class="container mx-auto text-center px-6 sm:flex sm:gap-6 mt-10 ">
+      <div class="rounded-md shadow-lg overflow-hidden mb-10  ">
+      <div><img src={foto} alt='vidio' class="w-full"></img></div>
+        <div class="px-6 py-2">
+          <div class="font-bold text-xl mb-2 text-slate-700 ">
+            title
+          </div>
+            <p class="text-sm text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, perferendis?
+            </p>
+          
+
+        </div>
+      </div>
+      <div class="rounded-md shadow-lg overflow-hidden mb-10 ">
+      <div><img src={foto} alt='vidio' class="w-full"></img></div>
+        <div class="px-6 py-2">
+          <div class="font-bold text-xl mb-2 text-slate-700 ">
+            title
+          </div>
+            <p class="text-sm text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, perferendis?
+            </p>
+          
+
+        </div>
+      </div>
+      <div class="rounded-md shadow-lg overflow-hidden mb-10">
+      <div><img src={foto} alt='vidio' class="w-full"></img></div>
+        <div class="px-6 py-2">
+          <div class="font-bold text-xl mb-2 text-slate-700 ">
+            title
+          </div>
+            <p class="text-sm text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, perferendis?
+            </p>
+          
+
+        </div>
+      </div>
+      <div class="rounded-md shadow-lg overflow-hidden mb-10">
+      <div><img src={foto} alt='vidio' class="w-full"></img></div>
+        <div class="px-6 py-2">
+          <div class="font-bold text-xl mb-2 text-slate-700 ">
+            title
+          </div>
+            <p class="text-sm text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, perferendis?
+            </p>
+          
+
+        </div>
+      </div>
+     </div>
+     <div class="bg-slate-400 dark:bg-gray-900">
+  <div class="grid grid-cols-2 gap-8 py-8 px-6 md:grid-cols-4">
+      <div>
+          <h2 class="mb-6 text-sm font-bold text-gray-900 uppercase dark:text-gray-400">INDOXII</h2>
+          <ul class="text-gray-500 dark:text-gray-400">
+              <li class="mb-4">
+                  <a href="#" class=" hover:underline text-black">Bioskop</a>
+              </li>
+              <li class="mb-4">
+                  <a href="#" class="hover:underline text-black">Terbaru</a>
+              </li>
+              <li class="mb-4">
+                  <a href="#" class="hover:underline text-black">Cinema INDOXII</a>
+              </li>
+              
+          </ul>
+      </div>
+      <div>
+          <h2 class="mb-6 text-sm font-bold text-gray-900 uppercase dark:text-gray-400">Film asia</h2>
+          <ul class="text-gray-500 dark:text-gray-400">
+              <li class="mb-4">
+                  <a href="#" class="hover:underline text-black">Thailand </a>
+              </li>
+              <li class="mb-4">
+                  <a href="#" class="hover:underline  text-black">China</a>
+              </li>
+              <li class="mb-4">
+                  <a href="#" class="hover:underline  text-black">Korea</a>
+              </li>
+             
+          </ul>
+      </div>
+      <div>
+          <h2 class="mb-6 text-sm font-bold text-gray-900 uppercase dark:text-gray-400">Cinema</h2>
+          <ul class="text-gray-500 dark:text-gray-400">
+              <li class="mb-4">
+                  <a href="#" class="hover:underline  text-black">action</a>
+              </li>
+              <li class="mb-4">
+                  <a href="#" class="hover:underline  text-black">horor</a>
+              </li>
+              <li class="mb-4">
+                  <a href="#" class="hover:underline  text-black">Drama</a>
+              </li>
+          </ul>
+      </div>
+      <div>
+          <h2 class="mb-6 text-sm font-bold text-gray-900 uppercase dark:text-gray-400">Striming</h2>
+          <ul class="text-gray-500 dark:text-gray-400">
+              <li class="mb-4">
+                  <a href="#" class="hover:underline text-black">Layarlebar</a>
+              </li>
+              <li class="mb-4">
+                  <a href="#" class="hover:underline  text-black">Dutafilm</a>
+              </li>
+              <li class="mb-4">
+                  <a href="#" class="hover:underline  text-black">Movie</a>
+              </li>
+             
+          </ul>
+      </div>
+  </div>
+  </div>
+
+</div>
+
+  );
+};
+
+export default Skills;
